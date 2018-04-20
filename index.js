9b8c33d77243628711a8ea7c173bcd531b49567f
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2018-04-20 15:09:16 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+express()
+    .use(express.static(path.join(__dirname, 'build')))
+    .use( errorHandler )
+    .listen( PORT, () => console.log("Listening on port =>", PORT))
+
+function errorHandler (err, req, res, next) {
+    console.error(err);
+    res.status(500).send("An error occurred!");
+}
